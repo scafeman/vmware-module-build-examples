c0f7adda897238205aa4dc244dad16df90890a55
Matt Scafe <matt.scafe@rackspace.com>
2023-04-19 17:43:58 -0500

    removed unused variables for the root tf files

--- vcd-network-routed-v2/main.tf
+++ vcd-network-routed-v2/main.tf
@@ -7,45 +7,14 @@ terraform {
   }
 }
 
-# Connect to the VMware Cloud Director Provider
-provider "vcd" {
-user                  = var.vcd_user
-password              = var.vcd_pass
-auth_type             = "integrated"
-org                   = var.vdc_org_name
-vdc                   = var.vdc_name
-url                   = var.vcd_url
-max_retry_timeout     = var.vcd_max_retry_timeout
-allow_unverified_ssl  = var.vcd_allow_unverified_ssl
-}
-
-
-# Create the Datacenter Group data source
-data "vcd_vdc_group" "dcgroup" {
-  name = var.vdc_group_name
-}
-
-# Create the NSX-T Edge Gateway data source
-data "vcd_nsxt_edgegateway" "t1" {
-  org      = var.vdc_org_name
-  owner_id = data.vcd_vdc_group.dcgroup.id
-  name     = var.vdc_edge_name
-}
 
 #########################
 # Org VDC Routed Network
 ######################### 
 
-module "routed-segment" {
+module "routed-org-network" {
   source          = "github.com/global-vmware/vcd-network-routed-v2"
   vdc_edge_name   = "1338829-US1-18916d0c-4c6d-42c3-be95-b911ee2119fb-edge"
   vdc_org_name    = "1338829-us1-rsvc-developmentenvironment"
   vdc_group_name  = "1338829-us1-rsvc-developmentenvironment datacenter group"
-  segment_name    = "Segment-01"
-  segment_gateway = "192.168.1.1"
-  start_address   = "192.168.1.51"
-  end_address     = "192.168.1.100"
-  dns1_address    = "69.20.0.164"
-  dns2_address    = "69.20.0.196"
-  dns_suffix      = "segment01.local"
 }
