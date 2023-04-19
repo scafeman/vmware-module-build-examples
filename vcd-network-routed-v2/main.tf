0b0af91574ed2e87659c1e419b2aab45a299d675
Matt Scafe <matt.scafe@rackspace.com>
2023-04-19 17:49:35 -0500

    updated module name to org-vdc-routed-network

--- vcd-network-routed-v2/main.tf
+++ vcd-network-routed-v2/main.tf
@@ -12,7 +12,7 @@ terraform {
 # Org VDC Routed Network
 ######################### 
 
-module "routed-org-network" {
+module "org-vdc-routed-network" {
   source          = "github.com/global-vmware/vcd-network-routed-v2"
   vdc_edge_name   = "1338829-US1-18916d0c-4c6d-42c3-be95-b911ee2119fb-edge"
   vdc_org_name    = "1338829-us1-rsvc-developmentenvironment"
