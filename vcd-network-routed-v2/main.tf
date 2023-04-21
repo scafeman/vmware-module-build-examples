7ae55f9cf530640123fd1266cc1789a830c0aef8
Matt Scafe <matt.scafe@rackspace.com>
2023-04-21 15:57:28 -0500

    Updated the DNS Servers to 192.168.1.5 and 192.168.1.6

--- vcd-network-routed-v2/main.tf
+++ vcd-network-routed-v2/main.tf
@@ -26,7 +26,7 @@ module "org_vdc_routed_network" {
   start_address       = "192.168.1.51"
   end_address         = "192.168.1.100"
 
-  dns1_address        = "192.168.1.10"
-  dns2_address        = "192.168.1.11"
+  dns1_address        = "192.168.1.5"
+  dns2_address        = "192.168.1.6"
   dns_suffix          = "mydomain.local"
 }
