ceccdd57209daad4398a769c8a71630396eb8fe5
Matt Scafe <matt.scafe@rackspace.com>
2023-04-21 13:22:10 -0500

    Added some additional Variable assignments into the vpp_vm module

--- vcd_vapp_vm/main.tf
+++ vcd_vapp_vm/main.tf
@@ -20,15 +20,23 @@ module "vapp_vm" {
   vdc_name                          = "1338829-us1-default-primary-vdc"
   vcd_edge_name                     = "1338829-US1-18916d0c-4c6d-42c3-be95-b911ee2119fb-edge"
   catalog_name                      = "1338829-US1-catalog"
+  catalog_template_name             = "Windows Server 2019"
+  vapp_org_network_name             = "US1-Segment-01"
+  network_cidr                      = "192.168.0.0/24" 
 
+  vm_count                          = 2
 
   vapp_name                         = "My Development Application"
-  vapp_org_network_name             = "US1-Segment-01"
-  catalog_template_name             = "Windows Server 2019"
   vm_name_environment               = "Dev"
+  vm_app_name                       = "MyApp"
+  vm_app_role                       = "Web Server"
   vm_computer_name_environment      = "dv"
-  vm_count                          = 1
-  network_cidr                      = "192.168.0.0/24"
+  vm_computer_name_app_name         = "myapp"
+  vm_computer_name_role             = "web"
+  
+  vm_metadata_role                  = "Web Server"
   vm_metadata_os                    = "Windows 2019"
+  vm_metadata_version               = "1.0"
+  vm_metadata_cost_center           = "IT Department-1001"
 }
 
