136c6c613bdbc25f25df1738af23b829efca2e52
Matt Scafe <matt.scafe@rackspace.com>
2023-04-24 18:57:10 -0500

    Added the new VM Metadata Entries into the vApp VM Module Example

--- vcd_vapp_vm/main.tf
+++ vcd_vapp_vm/main.tf
@@ -12,13 +12,13 @@ terraform {
 # Org VDC Routed Network
 ######################### 
 
-module "vapp_vm" {
-  source                            = "github.com/global-vmware/vcd_vapp_vm.git?ref=v1.3.0"
+module "vcd_vapp_vm" {
+  source                            = "github.com/global-vmware/vcd_vapp_vm.git?ref=v1.4.0"
   
   vdc_org_name                      = "1338829-us1-rsvc-developmentenvironment"
   vdc_group_name                    = "1338829-us1-rsvc-developmentenvironment datacenter group"
   vdc_name                          = "1338829-us1-default-primary-vdc"
-  vcd_edge_name                     = "1338829-US1-18916d0c-4c6d-42c3-be95-b911ee2119fb-edge"
+  vdc_edge_name                     = "1338829-US1-18916d0c-4c6d-42c3-be95-b911ee2119fb-edge"
   catalog_name                      = "1338829-US1-catalog"
   catalog_template_name             = "Ubuntu 22.04"
   vapp_org_network_name             = "US1-Segment-01"
@@ -26,7 +26,7 @@ module "vapp_vm" {
   network_ip_allocation_mode        = "MANUAL"
 
   vm_count                          = 2
-  vm_sizing_policy_name             = "gp4.8"
+  vm_sizing_policy_name             = "gp2.4"
 
   vapp_name                         = "Production BlazorApp vApp"
   vm_name_environment               = "Prod"
@@ -35,16 +35,45 @@ module "vapp_vm" {
   vm_computer_name_environment      = "pd"
   vm_computer_name_app_name         = "ba"
   vm_computer_name_role             = "web"
-  
-  vm_metadata_key_01                = "Cost Center"
-  vm_metadata_value_01              = "IT Department-1001"
-  vm_metadata_key_02                = "Operating System"
-  vm_metadata_value_02              = "Ubuntu 22.04"  
-  vm_metadata_key_03                = "Role"
-  vm_metadata_value_03              = "Web Server"
-  vm_metadata_key_04                = "Version"
-  vm_metadata_value_04              = "v1.0"  
 
+  vm_metadata_entries = [
+    {
+      key         = "Cost Center"
+      value       = "IT Department - 1001"
+      type        = "MetadataStringValue"
+      user_access = "READWRITE"
+      is_system   = false
+    },
+    {
+      key         = "Operating System"
+      value       = "Ubuntu Linux (64-Bit)"
+      type        = "MetadataStringValue"
+      user_access = "READWRITE"
+      is_system   = false
+    },
+    {
+      key         = "Role"
+      value       = "Web Server"
+      type        = "MetadataStringValue"
+      user_access = "READWRITE"
+      is_system   = false
+    },
+    {
+      key         = "Build Date"
+      value       = timestamp()
+      type        = "MetadataDateTimeValue"
+      user_access = "READWRITE"
+      is_system   = false
+    },
+    {
+      key         = "Built By"
+      value       = "Rackspace Build Automation Tool"
+      type        = "MetadataStringValue"
+      user_access = "READWRITE"
+      is_system   = false
+    }    
+  ]
+  
   vm_customization_force                  = "false"
   vm_customization_auto_generate_password = "true"
   
--- vcd_vapp_vm/outputs.tf
+++ vcd_vapp_vm/outputs.tf
@@ -1,47 +1,23 @@
 output "vm_names" {
-  value = "${module.vapp_vm.vm_names}"
+  value = module.vcd_vapp_vm.vm_names
+}
+
+output "vm_ips" {
+  value = module.vcd_vapp_vm.vm_ips
 }
 
 output "vm_computer_names" {
-  value = "${module.vapp_vm.vm_computer_names}"
+  value = module.vcd_vapp_vm.vm_computer_names
 }
 
-output "vm_metadata_key_01" {
-  value = module.vapp_vm.vm_metadata_key_01
+output "vm_metadata_entries" {
+  value = module.vcd_vapp_vm.vm_metadata_entries
 }
 
-output "vm_metadata_value_01" {
-  value = module.vapp_vm.vm_metadata_value_01
-}
-
-output "vm_metadata_key_02" {
-  value = module.vapp_vm.vm_metadata_key_02
-}
-
-output "vm_metadata_value_02" {
-  value = module.vapp_vm.vm_metadata_value_02
-}
-
-output "vm_metadata_key_03" {
-  value = module.vapp_vm.vm_metadata_key_03
-}
-
-output "vm_metadata_value_03" {
-  value = module.vapp_vm.vm_metadata_value_03
-}
-
-output "vm_metadata_key_04" {
-  value = module.vapp_vm.vm_metadata_key_04
-}
-
-output "vm_metadata_value_04" {
-  value = module.vapp_vm.vm_metadata_value_04
+output "vm_count" {
+  value = module.vcd_vapp_vm.vm_count
 }
 
 output "vm_sizing_policy_name" {
-  value = module.vapp_vm.vm_sizing_policy_name
-}
-
-output "vm_networks" {
-  value = "${module.vapp_vm.vm_networks}"
+  value = module.vcd_vapp_vm.vm_sizing_policy_name
 }
