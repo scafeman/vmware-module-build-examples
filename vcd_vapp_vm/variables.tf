6e40e244b00043f8724e5aec99c88539e336fe33
Matt Scafe <matt.scafe@rackspace.com>
2023-04-24 19:07:30 -0500

    Removed the Default Values for the Account Specific VDC Variables.  Moved them to the secrets.tfvars file

--- vcd_vapp_vm/variables.tf
+++ vcd_vapp_vm/variables.tf
@@ -22,19 +22,19 @@ variable "vcd_allow_unverified_ssl" {
 }
 variable "vdc_org_name" {
   description = "Cloud Director Organization Name"
-  default = "1338829-us1-rsvc-developmentenvironment"
+  default = ""
 }
 variable "vdc_name" {
   description = "Cloud Director VDC Name"
-  default = "1338829-us1-default-primary-vdc"
+  default = ""
 }
 variable "vdc_group_name" {
   description = "Cloud Director Datacenter Group Name"
-  default = "1338829-us1-rsvc-developmentenvironment datacenter group"
+  default = ""
 }
 variable "vdc_edge_name" {
   description = "Cloud Director Edge Name"
-  default = "1338829-US1-18916d0c-4c6d-42c3-be95-b911ee2119fb-edge"
+  default = ""
 }
 
 variable "admin_password" {
