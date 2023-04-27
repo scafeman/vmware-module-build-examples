5c0038b8161ee24edb57909fb40caf10e21402a7
Matt Scafe <matt.scafe@rackspace.com>
2023-04-27 11:31:50 -0500

    Added the vcd_vapp project to the vmware-modules-build-examples repo

---
+++ vcd_vapp/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.2"
+
+  required_providers {
+    vcd = {
+      source = "vmware/vcd"
+      version = "3.8.2"
+    }
+  }
+}
+
+module "vcd_vapp" {
+  source              = "github.com/scafeman/global-vmware-dev/vcd_vapp-v1.0"
+  
+  vdc_org_name        = "1338829-us1-rsvc-developmentenvironment"
+  vdc_group_name      = "1338829-us1-rsvc-developmentenvironment datacenter group"
+
+  vapp_names = [
+    "My Prod vApp-01",
+    "My Prod vApp-02"
+  ]
+}
