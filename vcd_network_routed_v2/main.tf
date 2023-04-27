4f3abb467c11453ed3dedf9e9dbb63ee12427c3e
Matt Scafe <matt.scafe@rackspace.com>
2023-04-27 11:01:41 -0500

    Adjusted the spacing in the module source line

--- vcd_network_routed_v2/main.tf
+++ vcd_network_routed_v2/main.tf
@@ -13,7 +13,7 @@ terraform {
 ######################### 
 
 module "org_vdc_routed_network" {
-  source          = "github.com/global-vmware/vcd_network_routed_v2.git?ref=v1.2.0"
+  source              = "github.com/global-vmware/vcd_network_routed_v2.git?ref=v1.2.0"
   
   vdc_edge_name       = "1338829-US1-18916d0c-4c6d-42c3-be95-b911ee2119fb-edge"
   vdc_org_name        = "1338829-us1-rsvc-developmentenvironment"
